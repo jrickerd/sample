88a8356e9dc44dda59383f6a96d6e74e93269b12
John Rickerd <john.rickerd@bodybuilding.com>
2025-07-16 07:40:14 -0400

    Removing NumberService reference.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReverseService } from './reverse/reverse.service';
 import { FibonacciService } from './fibonacci/fibonacci.service';
-import { NumberService } from './number/number.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, ReverseService, FibonacciService, NumberService],
+  providers: [AppService, ReverseService, FibonacciService],
 })
 export class AppModule {}
